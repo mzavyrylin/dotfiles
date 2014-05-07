70e79fee03706e01d436d3833c769d7c60fde0f9
Max <vandalsafe@gmail.com>
2014-05-07 13:14:08 +0400

    Add some env vars

--- zsh/0_env.zsh
+++ zsh/0_env.zsh
@@ -2,7 +2,9 @@ export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
 export RUBY_CFLAGS="-march=native -O2"
+export ARCHFLAGS="-arch x86_64"
 export PATH="$HOME/.rbenv/bin:$PATH:$HOME/bin"
 export TERM=xterm-256color
+export EDITOR=vim
 
 eval "$(rbenv init -)"
