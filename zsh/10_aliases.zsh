b21db2b31a7f513bb8fd14a7c398cfe44b3843b6
Max <vandalsafe@gmail.com>
2014-05-07 13:22:18 +0400

    Rework some aliases

--- zsh/10_aliases.zsh
+++ zsh/10_aliases.zsh
@@ -1,6 +1,18 @@
-alias bex="bundle exec"
-alias brake="bundle exec rake"
+alias b="bundle exec"
+alias rr="bundle exec rake"
+alias br="bundle exec rails"
 alias h="history"
-alias ll="ls -l --color"
-alias la="ls -laF --color"
-alias gg="grep"
+
+if [[ "`uname`" == "Darwin" ]]; then
+  alias ll="ls -l -G"
+  alias la="ls -laF -G"
+else
+  alias ll="ls -l --color"
+  alias la="ls -laF --color"
+fi
+
+alias gg="`which grep` --color=auto"
+alias gap="git add -p"
+alias gc="git commit -m"
+alias gf="git fetch -p"
+alias gum="git fetch -p && git checkout master && git pull && git checkout -"
