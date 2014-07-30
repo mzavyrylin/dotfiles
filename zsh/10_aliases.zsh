d89030c5ebbcc64138eb67e3ba04eada14afd971
Max Zavyrylin <vandalsafe@gmail.com>
2014-07-30 11:17:39 +0400

    Add some aliases for git

--- zsh/10_aliases.zsh
+++ zsh/10_aliases.zsh
@@ -11,8 +11,13 @@ else
   alias la="ls -laF --color"
 fi
 
+function fxp() { git commit --fixup=$1 }
+function sq()  { git commit --squash=$1 }
+function rbz() { git rebase -i $1 --autosquash }
+
 alias gg="`which grep` --color=auto"
 alias gap="git add -p"
 alias gc="git commit -m"
 alias gf="git fetch -p"
 alias gum="git fetch -p && git checkout master && git pull && git checkout -"
+alias zbs="git rebase --continue"
